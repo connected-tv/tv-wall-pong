3ab5f8ec3e36ede58343f094ee0944eed001e797
John Beech <john.beech@bbc.co.uk>
2015-06-15 18:04:29 +0100

    Added TV_WALL_PONG_SERVER_PORT as alternative port number, set in environment.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var monitor = require('product-monitor');
 
+var serverPort = process.env.TV_WALL_PONG_SERVER_PORT || 9000;
+
 var server = monitor({
   "productInformation": {
     "title": "TV Wall Pong"
   },
-  "serverPort": 9000,
+  "serverPort": serverPort,
   "userContentPath": "pong"
 });
