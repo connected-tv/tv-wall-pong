68cbf56210ce05f67444a2e07a98243c66000f91
John Beech <john.beech@bbc.co.uk>
2015-07-21 13:44:45 +0100

    Added in velocity

--- pong/api/tvPong.js
+++ pong/api/tvPong.js
@@ -7,21 +7,26 @@ instance.configure = function(config) {
 }
 
 instance.render = function(req, res) {
-  var clientId = req.params.id || false;
+    var clientId = req.params.id || false;
 
-  var data = {
-    ball: {
-      x: 0,
-      y: 0,
-      width: '10',
-      height: '10',
-      image: '/images/ball.png'
-    },
-    client: {
-      id: clientId
-    }
-  };
-  res.send(data);
+    var data = {
+        visibleItems: [{
+            name: 'ball',
+            x: 0,
+            y: 0,
+            width: '10',
+            height: '10',
+            image: '/images/ball.png',
+            velocity: {
+                x: -10,
+                y: 10
+            }
+        }],
+        client: {
+            id: clientId
+        }
+    };
+    res.send(data);
 }
 
 module.exports = instance;
