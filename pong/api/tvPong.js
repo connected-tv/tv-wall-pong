6194b754c206d2fc08005807c1ff7d553e055b4a
John Beech <john.beech@bbc.co.uk>
2015-07-23 16:31:06 +0100

    Refactored tvPong to bound ball position functions.

--- pong/api/tvPong.js
+++ pong/api/tvPong.js
@@ -4,56 +4,60 @@ instance.cacheDuration = "1 second";
 
 instance.route = '/api/client/:id/tv-pong.js';
 
+var visibleItems = [{
+  name: 'ball',
+  x: Math.floor(Math.random() * 400),
+  y: Math.floor(Math.random() * 400),
+  width: '10',
+  height: '10',
+  image: '/images/ball.png',
+  timestamp: Date.now(),
+  velocity: {
+    x: Math.floor(Math.random() * 51) - 25,
+    y: Math.floor(Math.random() * 51) - 25,
+  }
+}];
+
 instance.configure = function(config) {
-  self = this;
-  self.visibleItems = [{
-            name: 'ball',
-            x: Math.floor(Math.random() * 400),
-            y: Math.floor(Math.random() * 400),
-            width: '10',
-            height: '10',
-            image: '/images/ball.png',
-            timestamp: Date.now(),
-            velocity: {
-                x: Math.floor(Math.random() * 51) - 25,
-                y: Math.floor(Math.random() * 51) - 25,
-            }
-    }];
+
 }
 
 instance.render = function(req, res) {
-    var clientId = req.params.id || false;
+  var clientId = req.params.id || false;
 
-    var ball = self.visibleItems[0];
-    var t_now = Date.now();
-    var t_diff = (t_now - ball.timestamp)/1000;
-    ball.x = ball.x + ball.velocity.x * t_diff;
-    while ( ball.x > 500 || ball.x < 0 ) {
-      if ( ball.x > 500 ) {
-        ball.x = 2 * 500 - ball.x;
-      } else {
-        ball.x = - ball.x;
-      }
-      ball.velocity.x = - ball.velocity.x;
-    }
-    ball.y = ball.y + ball.velocity.y * t_diff;
-    while ( ball.y > 500 || ball.y < 0 ) {
-      if ( ball.y > 500 ) {
-        ball.y = 2 * 500 - ball.y;
-      } else {
-        ball.y = - ball.y;
-      }
-      ball.velocity.y = - ball.velocity.y;
-    }
-    ball.timestamp = t_now;
+  var ball = visibleItems[0];
 
-    var data = {
-        visibleItems: self.visibleItems,
-        client: {
-            id: clientId
-        }
-    };
-    res.jsonp(data);
+  boundBallPosition(ball);
+
+  var data = {
+    visibleItems: visibleItems,
+    client: {
+      id: clientId
+    }
+  };
+  res.jsonp(data);
+}
+
+function boundBallPosition(ball) {
+  var t_now = Date.now();
+  var t_diff = (t_now - ball.timestamp) / 1000;
+
+  boundBallAxis(ball, 'x', t_diff);
+  boundBallAxis(ball, 'y', t_diff);
+
+  ball.timestamp = t_now;
+}
+
+function boundBallAxis(ball, axis, t_diff) {
+  ball[axis] = ball[axis] + ball.velocity[axis] * t_diff;
+  while (ball[axis] > 500 || ball[axis] < 0) {
+    if (ball[axis] > 500) {
+      ball[axis] = 2 * 500 - ball[axis];
+    } else {
+      ball[axis] = -ball[axis];
+    }
+    ball.velocity[axis] = -ball.velocity[axis];
+  }
 }
 
 module.exports = instance;
